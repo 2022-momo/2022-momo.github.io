21244cc5b962a47a5abbd9c27f0b2664dbefb73a
Seongwon97 <osw970325@naver.com>
2022-09-05 23:56:07 +0900

    chore: google analytics tranking ID 업데이트

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -25,6 +25,6 @@ module.exports = {
   share: {
     facebookAppId: "", // Add facebookAppId for using facebook share feature v3.2
   },
-  ga: "UA-176158326-1", // Add your google analytics tranking ID
+  ga: "G-Y3VQCJ7ZEX", // Add your google analytics tranking ID
   // jennifer_front_id: "ed243e67", // Add your jennifer front tranking ID
 }
